5a5a493c1cf28eaf7b98813bd0c1ec4fdbf89fec
Jimmy2849 <jee9893@gmail.com>
2024-04-15 15:35:16 +0900

    add serializer

--- project/DB/serializers.py
+++ project/DB/serializers.py
@@ -1,4 +1,13 @@
 from rest_framework import serializers
 from DB.models import User, Dictionary
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class DictionarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dictionary
+        fields = '__all__' 
 
--- project/DB/views.py
+++ project/DB/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
