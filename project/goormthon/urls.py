f7cc337ac13e0b739c2c934e1e5c65f62c7c369a
Jimmy2849 <jee9893@gmail.com>
2024-04-15 15:33:59 +0900

    add app url

--- project/goormthon/urls.py
+++ project/goormthon/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('db/', include('DB.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
