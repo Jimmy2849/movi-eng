08e0b8d4212e7976345c34109abdaaed0b89ae0c
이호열 <125808654+hoyeol903@users.noreply.github.com>
2024-04-17 16:08:07 +0900

    Add files via upload

--- final_moving/pages/6_learningPage.py
+++ final_moving/pages/6_learningPage.py
@@ -1,35 +1,66 @@
-# 개인학습페이지
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import torch
+import streamlit as st
+from langchain.llms import OpenAI
+
 import streamlit as st
 import requests
 import re
 import random
-# from storyChat import story
-# from ragModel import ragmodel()
 
-#개인 학습 페이지
+
+
+# 모델 및 토크나이저 로드
+model_path = "baristarules/gec-model"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_path)
+
+# GPU 사용 설정
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
 # 저장된 단어 불러와서 뜻 맞추기
 url = "http://localhost:3000"
 
-# 단어 불러오기
-def wordQuiz():
+# 문장 교정 불러오기
+def correct_grammar(input_sentence):
 
-    if st.button('단어 불러오기'):
-        token = load_token_from_local_storage() # 토큰 불러오기
-        if token:
+    inputs = tokenizer(input_sentence, return_tensors="pt", padding=True, truncation=True, max_length=512)
+
+    input_ids = inputs.input_ids.to(device)
+    attention_mask = inputs.attention_mask.to(device)
+
+    outputs = model.generate(input_ids, attention_mask=attention_mask, max_length=512)
+    corrected_sentence = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return corrected_sentence
+
+# 단어 불러오기 및 문장 생성
+def wordQuiz():
+    
+    st.title("단어 학습장")
+
+    token = load_token_from_local_storage() # 토큰 불러오기
+    if token:
             headers = {"Authorization": f"Bearer {token}"}
             response = requests.get(url + "/send_word", headers=headers)
             if response.status_code == 200:
-                word = response.text
-                # st.write("단어 뜻 맞추기입니다.")
-                # st.write(word)
+                word = response.text        
                 Quiz(word)
+                
+                sentence()
+
+                
             else:
                 # 저장된 단어가 없을 경우
                 error_msg = response.json().get('error')
                 st.error(error_msg)
-        else:
+    else:
             st.warning("로컬 스토리지에 저장된 토큰이 없습니다.")
             st.write("로그인을 먼저 진행해주세요.")    
+
+
 
 # 단어 퀴즈 구현
 def Quiz(word):
@@ -43,28 +74,75 @@ def Quiz(word):
             englist.append(i)
         else:
             kolist.append(i)
-    # st.write(englist,kolist)
-    random_en_word = random.choice(englist)
-    random_ko_word = random.choice(kolist)
+
+    if 'word_index' not in st.session_state:
+        st.session_state['word_index'] = []
+
+    en_random_index = random.randint(0, min(len(englist), len(kolist)) - 1)
+    ko_random_index = random.randint(0, min(len(englist), len(kolist)) - 1)
+    
+    if st.button('다음 단어'):
+        st.session_state.word_index.append((englist[en_random_index], kolist[ko_random_index], en_random_index, ko_random_index))
+        if len(st.session_state.word_index) > 2:
+            st.session_state.word_index.pop(0)
+    
+
 
     col1, col2 = st.columns(2)
     with col1:
-        st.info(random_en_word)
+        try:
+            st.info(st.session_state.word_index[0][0])
+        except IndexError:
+            pass
+            
+
+
     with col2:
-        st.info(random_ko_word)
+        try:
+            st.info(st.session_state.word_index[0][1])
+        except IndexError:
+            pass
 
-    # 두 index를 비교하여 정답 확인 맞았을 경우 비슷한 문장 생성과 해설
-    st.button('뜻이 서로 같습니다.')
-    st.button('뜻이 서로 다릅니다.')
 
+
+    genre = st.checkbox('단어의 뜻이 서로 같습니다.')
+    no_genre = st.checkbox('단어의 뜻이 서로 다릅니다.')
+
+    if genre:
+        if st.session_state.word_index[0][2] == st.session_state.word_index[0][3]:
+         st.success(f'정답입니다! {st.session_state.word_index[0][0]}는 "{st.session_state.word_index[0][1]}" 의미를 가지고 있습니다')
+        else:
+            st.warning(f'아쉬워요.. 다시 잘 생각해보세요')
+    
+    if no_genre:
+        if st.session_state.word_index[0][2] != st.session_state.word_index[0][3]:
+         st.success(f'정답입니다! {st.session_state.word_index[0][0]}는 "{st.session_state.word_index[0][1]}" 의미를 가지고 있습니다')
+        else:
+            st.warning(f'아쉬워요.. 다시 잘 생각해보세요')
     
 
 
 
-
 # 문장 교정
-def sentenceCorrection():
+def sentence():
+
+    llm = OpenAI(api_key="")
     st.header("문장 교정")
+
+    # st.divider()
+    input_sentence = st.text_input("위 단어를 가지고 문장을 만들어 보세요!")
+
+    on = st.toggle('도움이 필요하신가요?')
+    if on:
+        st.write('다음 문장을 활용해 보세요!')
+        result = st.session_state.word_index[0][0]
+        result = llm.predict("Make 1 short and simple English sentences using" + result)
+        st.text(result)
+
+    if st.button("문장 교정"):
+        corrected_sentence = correct_grammar(input_sentence)
+        st.text("Original:" + input_sentence)
+        st.text("Corrected:" + corrected_sentence)
 
 def load_token_from_local_storage():
     # 세션 상태에서 토큰을 불러오는 함수
@@ -75,10 +153,9 @@ def main():
     token = load_token_from_local_storage()
 
     if token:
-        st.success("반갑습니다 :)")
         wordQuiz()
-        # sentenceCorrection()
-
+        # sentence()
+            
     else:
         st.warning("로컬 스토리지에 저장된 토큰이 없습니다.")
         st.write('로그인을 먼저 진행해주세요.')
