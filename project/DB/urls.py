1e983c484ac3540ffe6735f613df6fc2404463a2
Jimmy2849 <jee9893@gmail.com>
2024-04-19 03:10:36 +0900

    set view function to url

--- project/DB/urls.py
+++ project/DB/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import custom_signup, custom_login
+from . import views
 
 app_name = 'db' # 네임스페이스 추가. streamlit에서 접근 시 'http://your-django-server/<app_namespace>/<endpoint>/' 처럼 직접 지정.
 urlpatterns = [
-    path('/signup', custom_signup),
-    path('/login/', custom_login),
+    path('signup', views.custom_signup, name='signup'),
+    path('login/', views.custom_login, name='login'),
 ]
