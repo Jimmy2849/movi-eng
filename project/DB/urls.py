3e75b2d0840319b37dd135615e3dbd3a6467853a
Jimmy2849 <jee9893@gmail.com>
2024-04-25 05:57:41 +0900

    add save_word

--- project/DB/urls.py
+++ project/DB/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('signup/', views.custom_signup, name='signup'),
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
+    path('save_word/', views.save_word, name='save_word'),
+    path('select_word/', views.select_word, name='select_word'),
 ]
--- project/DB/views.py
+++ project/DB/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from DB.models import Dictionary
 
 # Create your views here.
 # 사용자가 입력한 username를 data로 받음, 응답으로 'success'와 'token'을 json으로 반환해야 함.
@@ -30,14 +31,12 @@ def custom_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         # 회원명을 기반으로 사용자를 검색합니다.
         user = authenticate(username=username, password=password)
-
         if user is not None:
             # 사용자를 로그인합니다.
             login(request, user)
-            return JsonResponse({'success': True}, status=200)
+            return JsonResponse({'success': True, 'userid': userid}, status=200)
         else:
             return JsonResponse({'success': False, 'error': 'Invalid ID.'}, status=400)
 
@@ -50,3 +49,28 @@ def custom_logout(request):
         return JsonResponse({'success': True}, status=200)
     else:
         return JsonResponse({'success': False, 'error': 'Method not allowed.'}, status=405)
+
+# username으로 userid를 찾아 외래키에 저장
+# 중복된 단어검색 소요 시간 -> 인덱스?
+@csrf_exempt
+def save_word(request):
+    if request.method == 'POST':
+        word = request.POST.get('word')
+        meaning = request.POST.get('meaning')
+        userid = request.POST.get('userid')
+        print(f'userid: {userid}\n word: {word}\n meaning: {meaning}\n')
+        word_obj, created = Dictionary.objects.get_or_create(word=word, userid=User(id=userid), defaults={"meaning": meaning},)
+        if created:
+            print("created: True")
+            word_obj.save()
+            return JsonResponse({'success': True, 'message': f"단어 '{word}'를 데이터베이스에 저장했습니다."})
+        else:
+            return JsonResponse({'success': False, 'message': f"단어 '{word}'는 이미 데이터베이스에 존재합니다."})
+    return
+
+# 참조하는 userid에 해당하는 단어장 출력
+# Select * from Dict
+# Select Count(*) from Dict
+@csrf_exempt
+def select_word(request):
+    return Dictionary.objects.all()
